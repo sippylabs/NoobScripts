12fb5359393736b787efd579859c09ef9c55a53b
Sippy <sippy@sippylabs.com>
2015-10-18 12:40:04 +0100

    New reset path for Left side

--- oldschool/scripts/NoobCrabs/Enums/Location.java
+++ oldschool/scripts/NoobCrabs/Enums/Location.java
@@ -6,7 +6,7 @@ import org.powerbot.script.Tile;
 public enum Location {
     LEFT(
             new Area(new Tile(2662, 3734, 0), new Tile(2688, 3712, 0)),
-            new Tile[]{new Tile(2673, 3710), new Tile(2674, 3702), new Tile(2673, 3694)}
+            new Tile[]{new Tile(2678, 3720), new Tile(2674, 3712), new Tile(2674, 3701), new Tile(2674, 3692), new Tile(2673, 3684)}
     ),
     RIGHT(
             new Area(new Tile(2692, 3730, 0), new Tile(2720, 3713, 0)),
