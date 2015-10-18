b436e0d4d1022afc5bd0a694d7e60663ae4f21f2
Sippy <sippy@sippylabs.com>
2015-10-19 00:11:54 +0100

    Rename package

---
+++ oldschool/scripts/noobcrabs/tasks/Eat.java
@@ -0,0 +1,48 @@
+package oldschool.scripts.NoobCrabs.Tasks;
+
+import oldschool.scripts.Common.Utilities.Task;
+import oldschool.scripts.NoobCrabs.NoobCrabs;
+import org.powerbot.script.Condition;
+import org.powerbot.script.Filter;
+import org.powerbot.script.rt4.ClientContext;
+import org.powerbot.script.rt4.Item;
+
+import java.util.Arrays;
+
+public class Eat extends Task<ClientContext> {
+    private double eatAtPercentage;
+
+    public Eat(ClientContext ctx, double eatAtPercentage) {
+        super(ctx);
+        this.eatAtPercentage = eatAtPercentage;
+    }
+
+    @Override
+    public boolean activate() {
+        return ctx.game.loggedIn()
+                && !ctx.inventory.select().select(new Filter<Item>() {
+            @Override
+            public boolean accept(Item gameObject) {
+                return Arrays.asList(gameObject.actions()).contains("Eat");
+            }
+        }).isEmpty()
+                && ctx.players.local().health() < ctx.players.local().maxHealth() * eatAtPercentage;
+    }
+
+    @Override
+    public void execute() {
+        NoobCrabs.status = "Nomming ...";
+
+        Item foodz = ctx.inventory.select(new Filter<Item>() {
+            @Override
+            public boolean accept(Item gameObject) {
+                return Arrays.asList(gameObject.actions()).contains("Eat");
+            }
+        }).first().poll();
+
+        if (foodz.valid())
+            foodz.interact("Eat");
+
+        Condition.sleep();
+    }
+}
