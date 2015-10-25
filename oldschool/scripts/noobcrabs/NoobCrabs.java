8b1d4e1228db5f6001ffb0cb0c3e0233c2cad04f
Sippy <sippy@sippylabs.com>
2015-10-25 23:14:34 +0000

    Move startup variable assignment into GUI

--- oldschool/scripts/noobcrabs/NoobCrabs.java
+++ oldschool/scripts/noobcrabs/NoobCrabs.java
@@ -4,16 +4,10 @@ import oldschool.scripts.common.Task;
 import oldschool.scripts.noobcrabs.enums.Location;
 import oldschool.scripts.noobcrabs.gui.Paint;
 import oldschool.scripts.noobcrabs.gui.StartupInterface;
-import oldschool.scripts.noobcrabs.tasks.Attack;
-import oldschool.scripts.noobcrabs.tasks.Eat;
-import oldschool.scripts.noobcrabs.tasks.Find;
-import oldschool.scripts.noobcrabs.tasks.Reset;
 import org.powerbot.script.PaintListener;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script;
-import org.powerbot.script.Tile;
 import org.powerbot.script.rt4.ClientContext;
-import org.powerbot.script.rt4.Constants;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -21,7 +15,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 
 @Script.Manifest(
@@ -41,33 +34,9 @@ public class NoobCrabs extends PollingScript<ClientContext> implements PaintList
 
     @Override
     public void start() {
-        start.atkxp = ctx.skills.experience(Constants.SKILLS_ATTACK);
-        start.strxp = ctx.skills.experience(Constants.SKILLS_STRENGTH);
-        start.defxp = ctx.skills.experience(Constants.SKILLS_DEFENSE);
-        start.hpxp = ctx.skills.experience(Constants.SKILLS_HITPOINTS);
-
-        if (Location.LEFT.area().contains(ctx.players.local()))
-            location = Location.LEFT;
-        else if (Location.RIGHT.area().contains(ctx.players.local()))
-            location = Location.RIGHT;
-        else {
-            Tile player = ctx.players.local().tile();
-            location = (player.distanceTo(Location.LEFT.area().getCentralTile())
-                    > player.distanceTo(Location.RIGHT.area().getCentralTile()))
-                    ? Location.RIGHT : Location.LEFT;
-        }
-
-        tasks.addAll(Arrays.asList(
-                        new Find(ctx)
-                        , new Reset(ctx)
-                        , new Attack(ctx, start.killSteal)
-                        , new Eat(ctx, start.eatAtPercentage)
-                        /*,new Hop(ctx)*/
-                )
-        );
-
-        StartupInterface dialog = new StartupInterface(ctx, start);
+        StartupInterface dialog = new StartupInterface(ctx, start, tasks);
         dialog.pack();
+        dialog.setMinimumSize(dialog.getSize());
         dialog.setVisible(true);
     }
 
@@ -93,7 +62,7 @@ public class NoobCrabs extends PollingScript<ClientContext> implements PaintList
     private void screenShot() {
         final int width = ctx.game.dimensions().width, height = ctx.game.dimensions().height;
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        // your paint's repaint(Graphics) method
+
         repaint(img.createGraphics());
         img = img.getSubimage(5, 5, 140, 180);
         final File screenshot = new File(getStorageDirectory(), ctx.controller.script().getName() + "_"
--- oldschool/scripts/noobcrabs/Startup.java
+++ oldschool/scripts/noobcrabs/Startup.java
@@ -8,14 +8,25 @@ public class Startup {
     public int defxp;
     public int hpxp;
     public boolean killSteal;
+    public boolean pure;
+
+    public boolean hoppingEnabled;
+    public int maxPlayersInArea;
+    public int maxWorldsToHop;
 
     public Startup() {
         startTime = 0;
-        eatAtPercentage = 0.5;
         atkxp = 0;
         strxp = 0;
         defxp = 0;
         hpxp = 0;
+
+        eatAtPercentage = 0.5;
         killSteal = false;
+        pure = false;
+
+        hoppingEnabled = false;
+        maxPlayersInArea = 8;
+        maxWorldsToHop = 10;
     }
 }
