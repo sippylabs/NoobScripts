615a2e77c2f9998e7fb1a31d55f59502a5840b9a
Sippy <sippy@sippylabs.com>
2015-10-31 21:02:18 +0000

    Automatic uploading of progress reports

--- oldschool/scripts/noobcrabs/NoobCrabs.java
+++ oldschool/scripts/noobcrabs/NoobCrabs.java
@@ -1,5 +1,6 @@
 package oldschool.scripts.noobcrabs;
 
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 import oldschool.scripts.common.Task;
 import oldschool.scripts.noobcrabs.enums.Location;
 import oldschool.scripts.noobcrabs.gui.Paint;
@@ -10,9 +11,14 @@ import org.powerbot.script.Script;
 import org.powerbot.script.rt4.ClientContext;
 
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -64,6 +70,9 @@ public class NoobCrabs extends PollingScript<ClientContext> implements PaintList
     private void screenShot() {
         final int width = ctx.game.dimensions().width, height = ctx.game.dimensions().height;
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        final String albumId = "75nXaJFy979q9z1";
+        final String imgurKey = "89fa33eb76e11fe";
+        final String uploadUrl = "https://api.imgur.com/3/image";
 
         repaint(img.createGraphics());
         img = img.getSubimage(5, 5, 140, 180);
@@ -73,6 +82,32 @@ public class NoobCrabs extends PollingScript<ClientContext> implements PaintList
             ImageIO.write(img, "png", screenshot);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        try {
+            System.setProperty("https.protocols", "TLSv1");
+            final ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+            ImageIO.write(img, "png", byteArray);
+
+            final String imageData = Base64.encode(byteArray.toByteArray());
+            final String image = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(imageData, "UTF-8");
+            final String album = URLEncoder.encode("album", "UTF-8") + "=" + URLEncoder.encode(albumId, "UTF-8");
+            final String type = URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("base64", "UTF-8");
+            final String data = image + "&" + album + "&" + type;
+
+            HttpsURLConnection conn = (HttpsURLConnection) new URL(uploadUrl).openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Client-ID " + imgurKey);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.connect();
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
